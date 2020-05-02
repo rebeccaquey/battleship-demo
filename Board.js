7b138337e3c71d2def0c6f2037935e885ddb32b9
rebeccaquey <rebecca_quey@yahoo.com>
2020-05-02 16:13:26 -0700

    update row and maxcolstartpoint so that it is inclusive of all possibilities

--- Board.js
+++ Board.js
@@ -21,9 +21,11 @@ class Board {
     const shipLength = Math.floor((Math.random() * (this.rows[0].length - 1))) + 2;
     const shipName = 's' + shipLength;
 
-    const row = Math.floor(Math.random() * 4);
+    //change the number to be this.rows.length so it isn't hard coded to a certain number
+    const row = Math.floor(Math.random() * this.rows.length);
 
-    const maxColStartPoint = this.rows.length - shipLength;
+    //math.random returns a random number in the range of 0 to less than 1 (not inclusive of 1) so we need to add 1 to our maxcolstartpoint so when you math.random it, it could still include the rowlength - shiplength
+    const maxColStartPoint = this.rows.length - shipLength + 1;
     const colStartPoint = Math.floor(Math.random() * maxColStartPoint);
 
     for (let col = colStartPoint; col < colStartPoint + shipLength; col++) {
